e3a28eceda5228195a293093ed30248422970726
Carlos Barbosa <carloshenriquefb@gmail.com>
2018-11-03 11:13:48 -0300

    debounceTime added

--- public/app/app.js
+++ public/app/app.js
@@ -2,16 +2,10 @@ import { handleStatus, log } from './utils/promise-helpers.js' ;
 import './utils/array-helpers.js' ;
 // importa dando um apelido para o artefato importado
 import { notasService as service } from './nota/service.js' ;
-import { takeUntil } from './utils/operators.js' ;
+import { takeUntil, debounceTime } from './utils/operators.js' ;
 
-const operation1 = takeUntil( 3 , () => service .sumItems( '2143' ) .then(log) .catch(log) );
+const action = debounceTime( 500 , takeUntil( 3 , () => 
+    service .sumItems( '2143' ) .then(log) .catch(log) 
+    ));
 
-document .querySelector( '#myButton' )
-    .onclick = () => 
-    // utilizando o serviço 
-    service
-    .sumItems('2143')
-    .then(log)
-    .catch(log);
-
-document .querySelector( '#myButton' ) .onclick = operation1;
+document .querySelector( '#myButton' ) .onclick = action;
