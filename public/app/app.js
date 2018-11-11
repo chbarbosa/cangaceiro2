300ccb9464e48f2dc1dbb571d22c9ee121ba01f2
Carlos Barbosa <carloshenriquefb@gmail.com>
2018-11-11 09:42:48 -0300

    EventEmitter works!

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,7 @@ import './utils/array-helpers.js' ;
 // importa dando um apelido para o artefato importado
 import { notasService as service } from './nota/service.js' ;
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js' ;
+import { EventEmitter } from './utils/event-emitter.js' ;
 
 const operations = pipe( partialize(takeUntil, 3 ), partialize(debounceTime, 500 ) );
 
@@ -10,12 +11,7 @@ const operations = pipe( partialize(takeUntil, 3 ), partialize(debounceTime, 500
 const action = operations(() =>
     retry( 3 , 3000 , () =>
     timeoutPromise( 200 , service.sumItems( '2143' )))
-    .then(soma => {
-        console .log(soma);
-        // um interessado 
-        alert(soma); 
-        // outro interessado 
-    })
+    .then(total => EventEmitter.emit( 'itensTotalizados' , total))
     .catch(log) );
 
 document .querySelector( '#myButton' ) .onclick = action;
